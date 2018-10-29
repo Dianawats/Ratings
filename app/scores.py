9d74bf2228c8c2e864456a1ff5031cd9daa93b1b
whiz25 <afanimartin@gmail.com>
2018-10-29 11:31:47 +0300

    Add scores module. A file that shows all or one score

---
+++ app/scores.py
@@ -0,0 +1,10 @@
+class Scores:
+    def __init__(self):
+        self.scores = []
+
+    def all_scores(self):
+        for score in self.scores:
+            return score
+
+    def return_one_score(self, username):
+        my_score = [score for score in self.scores if user['username'] == username]
